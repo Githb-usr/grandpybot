000005088c12e2ab05b459457012e51495bfc79a
Githb-usr <tragopan@free.fr>
2021-05-16 14:39:08 +0200

    Ajout docstrings et commentaires sur le code

--- app/api/wiki_api.py
+++ app/api/wiki_api.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# import os
 import requests
 import time
 from dotenv import dotenv_values
@@ -14,53 +13,64 @@ class WikiApi:
         WikiApi class
         To manage the Wikipedia API data recovery
     """
+
     def __init__(self):
         """ Constructor """
         self.parser = Parser()
 
     def get_page_title(self, coordinates):
         """
-            We get xxx
-            :return: xxx
-            :rtype: xxx
+            We get the title of the Wikipedia page
+            :param: coordinates is a tuple with latitude and longitude of the
+            place from map API.
+            :return: the page title and place coordinates from Wikipedia
+            :rtype: dictionary
         """
-        if coordinates != None:
-            params = {
-                "action": "query",
-                "list": "geosearch",
-                "format": "json",
-                "gscoord": str(coordinates[0])+"|"+str(coordinates[1]),
-                "gsradius": 250,
-                "gslimit": 1,
-                "utf8":''
-            }
-            result = requests.get(WIKI_API_URL, params = params)
+        # We define the parameters of the request
+        params = {
+            "action": "query",
+            "list": "geosearch",
+            "format": "json",
+            "gscoord": str(coordinates[0])+"|"+str(coordinates[1]),
+            "gsradius": 250,
+            "gslimit": 1,
+            "utf8":''
+        }
+        result = requests.get(WIKI_API_URL, params = params)
 
-            if result.status_code == 200:
-                raw_data = result.json()
-                try:
-                    data = raw_data['query']['geosearch'][0]
-                    parsed_data_title = self.parser.get_parsed_data(data['title'])
+        # If the request succeeds, we we retrieve only the data we are
+        # interested in among all those present in the JSON obtained
+        if result.status_code == 200:
+            raw_data = result.json()
+            try:
+                data = raw_data['query']['geosearch'][0]
 
-                    return (data['title'], (data['lat'], data['lon']))
-                except ValueError:
-                    return 'savapa'
-            else:
-                print("La connexion à l'API de Wikipédia a échoué.")
-                print("Nouvelle tentative.")
-                time.sleep(1)
-                self.get_wikipedia_data()
+                return {"page_title": data['title'], "latitude": data['lat'],
+                        "longitude": data['lon']}
+            except ValueError:
+                return 'savapa'
+        # If the request fails, we restart it after 1 second of waiting.
         else:
-            print('pas de coordonnées')
+            print("La connexion à l'API de Wikipédia a échoué.")
+            print("Nouvelle tentative.")
+            time.sleep(1)
+            self.get_wikipedia_data()
 
     def get_wikipedia_data(self, coordinates):
         """
-            We recover an extract of Wikipedia page about the cleaned string.
-            :return: dictionnary data
-            :rtype: dict()
+            We recover an extract of Wikipedia page about the cleaned string
+            from the API.
+            :param: coordinates is a tuple with latitude and longitude of the
+            place from map API.
+            :return: the page title, the page extract and the wikipedia
+            coordinates of the place, often more accurate than the map API.
+            :rtype: dictionary
         """
-        page_title = self.get_page_title(coordinates)
+        # We get the title of the Wikipedia page to build the query.
+        wiki_title_coord = self.get_page_title(coordinates)
+        page_title = wiki_title_coord["page_title"]
 
+        # We define the parameters of the request
         params = {
             "action": "query",
             "format": "json",
@@ -68,18 +78,25 @@ class WikiApi:
             "exchars": 1200,
             "exintro": 1,
             "explaintext": 1,
-            "titles": page_title[0]
+            "titles": page_title
         }
         result = requests.get(WIKI_API_URL, params = params)
-        # print(result.url)
+
+        # If the request succeeds, we build a dictionary containing the
+        # following 3 pieces of information: title of the page, excerpt of
+        # the page and coordinates proposed by Wikipedia
         if result.status_code == 200:
             data = result.json()
             wiki_title = list(data['query']['pages'].values())[0]['title']
             wiki_extract = list(data['query']['pages'].values())[0]['extract']
-            wiki_coord = (page_title[1][0], page_title[1][1])
+            wiki_coord = (wiki_title_coord["latitude"],
+                          wiki_title_coord["longitude"])
 
-            return {"wiki_title": wiki_title, "wiki_extract": wiki_extract, "wiki_coord": wiki_coord}
+            return {"wiki_title": wiki_title, "wiki_extract": wiki_extract,
+                    "wiki_coord": wiki_coord}
+        # If the request fails, we restart it after 1 second of waiting.
         else:
             print("La connexion à l'API de Wikipédia a échoué.")
             print("Nouvelle tentative.")
+            time.sleep(1)
             self.get_wikipedia_data()
